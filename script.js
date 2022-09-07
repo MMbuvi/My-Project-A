dce754f21294bf1af6fd1080a8e1fc47dd63b6ee
Meshack <mbuvi.meshack99@gmail.com>
2022-09-08 01:29:12 +0300

    nineth commit

---
+++ script.js
@@ -0,0 +1,3 @@
+const apiURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d7af6e27ee729d2bdb078dda2e27569c&page=1';
+const IMGPATH = "https://image.tmdb.org/t/p/w500"
+const SEARCHAPI = 'https://api.themoviedb.org/3/discover/movie?&api_key=d7af6e27ee729d2bdb078dda2e27569c&qury='
