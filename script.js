c2e3429e1a6b1c96cbe547c3dc19fbe509ac187a
Meshack <mbuvi.meshack99@gmail.com>
2022-09-08 08:54:50 +0300

    twelveth commit

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const apiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d7af6e27ee729d2bdb078dda2e27569c&page=1';
 const IMGPATH = "https://image.tmdb.org/t/p/original";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=d7af6e27ee729d2bdb078dda2e27569c&query="
+
+const main = document.getElementById("main")
+const form = document.getElementById("form")
+const search = document.getElementById("search")
+
